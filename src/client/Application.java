ea0b192dc65e07dd7788fca611f52663a0ac430d
Devistorm <quentlace2g@gmail.com>
2022-04-01 23:09:12 +0200

    Création de la classe Application du client.

---
+++ src/client/Application.java
@@ -0,0 +1,23 @@
+package client;
+
+import processing.core.PApplet;
+
+public class Application extends PApplet {
+
+    public static void main(String[] args) {
+        PApplet.main("client.Application");
+    }
+
+    @Override
+    public void setup() {
+    }
+
+    @Override
+    public void settings() {
+        size(1000, 700);
+    }
+
+    @Override
+    public void draw() {
+    }
+}
